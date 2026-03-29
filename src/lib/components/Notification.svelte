069a30a437ae649345fe1c080e4c3009f8b4be08
Narley <gnarlynarley@gmail.com>
2026-03-29 15:24:54 +0200

    feat: add duration animation

--- src/lib/components/Notification.svelte
+++ src/lib/components/Notification.svelte
@@ -32,20 +32,46 @@
   class="notification"
   class:error={notification.type === "error"}
   onclick={remove}
+  style={`--duration: ${notification.duration}`}
 >
   {notification.text}
 </button>
 
 <style>
   .notification {
+    --duration: 0;
     background-color: var(--color-primary);
     padding: var(--gutter);
     color: var(--color-background);
     border-radius: var(--border-radius);
+    position: relative;
+    overflow: hidden;
+
+    &::after {
+      content: "";
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      display: block;
+      width: 100%;
+      height: 2px;
+      background: var(--color-background);
+      transform-origin: bottom left;
+      animation: shrink calc(var(--duration) * 1ms) forwards linear;
+    }
 
     &.error {
       background-color: var(--color-error);
       color: var(--color-error-text);
+    }
+  }
+
+  @keyframes shrink {
+    0% {
+      scale: 1 1;
+    }
+    100% {
+      scale: 0 1;
     }
   }
 </style>
