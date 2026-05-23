44571e121481865dcacbc89edf28ef26895635ee
Narley <gnarlynarley@gmail.com>
2026-05-23 15:17:48 +0200

    chore: remove meme

--- src/lib/components/Charr.svelte
+++ src/lib/components/Charr.svelte
@@ -1,33 +1,10 @@
 <script lang="ts">
   import phoebeSrc from "./Charr.image.phoebe.png";
-  import memeSrc from "./Charr.image.meme.png";
-  import swaggatarSrc from "./Charr.image.swaggatar.jpg";
-
-  const now = new Date();
-  const memeImages = [memeSrc, swaggatarSrc];
-  const isAprilFools = now.getMonth() === 3 && now.getDate() === 1;
-  const hasMeme = isAprilFools && Math.random() < 0.5;
 
   let src = $state(phoebeSrc);
-
-  if (hasMeme) {
-    src = memeImages[Math.floor(Math.random() * memeImages.length)];
-  }
-
-  $effect(() => {
-    if (!hasMeme) return;
-
-    const id = setTimeout(() => {
-      src = phoebeSrc;
-    }, 200);
-
-    return () => {
-      clearTimeout(id);
-    };
-  });
 </script>
 
-<img class="charr" {src} alt="Silly lil charr" />
+<img class="charr" {src} alt="A bird to watch while you wait" />
 
 <style>
   .charr {
